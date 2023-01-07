5ae52054d5cf747ad8c7db10bf6523e0e27faa74
fhraju <alonghuman@gmail.com>
2023-01-07 13:19:27 +0600

    Login With User Role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+      
+        $this->call(RoleSeeder::class);
+        $this->call(AdminSeeder::class);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ])->assignRole('user');
+
     }
 }
